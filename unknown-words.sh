4697acb1d58cc126703241dc71ad8b92fd6192f4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-01 09:43:38 -0400

    unknown-words: Fix standalone handling for summary

--- unknown-words.sh
+++ unknown-words.sh
@@ -12,10 +12,17 @@ export spellchecker="${spellchecker:-$THIS_ACTION_PATH}"
 
 basic_setup() {
   if [ -z "$GITHUB_ENV" ]; then
-    export GITHUB_EVENT_NAME=push
     export GITHUB_EVENT_PATH=$(mktemp)
     echo '{}' > "$GITHUB_EVENT_PATH"
-    export GITHUB_ENV=/dev/stderr
+
+    maybe_set_env() {
+      if [ -z "$(eval echo '$'"$1")" ]; then
+        eval $1="$2"
+      fi
+    }
+    maybe_set_env GITHUB_EVENT_NAME push
+    maybe_set_env GITHUB_ENV /dev/stderr
+    maybe_set_env GITHUB_STEP_SUMMARY /dev/stderr
   fi
 
   if ! command -v act-summary > /dev/null; then
