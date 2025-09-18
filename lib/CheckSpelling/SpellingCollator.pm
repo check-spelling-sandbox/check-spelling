dbde53beca02ceaf93f9c25a2ff5b58e98808114
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-18 16:25:37 -0400

    Use wrap_in_backticks

--- lib/CheckSpelling/SpellingCollator.pm
+++ lib/CheckSpelling/SpellingCollator.pm
@@ -547,10 +547,13 @@ sub main {
         chomp $warning;
         next unless $warning =~ s/^:(\d+)/:1/;
         $file = $check_file_paths[$1];
-        if ($warning =~ s/:(\d+ \.\.\. \d+): `(.*)`/:$1, Warning - `$2` is not a recognized word. (check-file-path)/) {
-          next if skip_item($2);
+        if ($warning =~ m/:(\d+ \.\.\. \d+): `(.*)`/) {
+          my ($range, $item) = ($1, $2);
+          my $wrapped = CheckSpelling::Util::wrap_in_backticks($item);
+          $warning =~ s/:\d+ \.\.\. \d+: `.*`/:$range, Warning - $wrapped is not a recognized word. (check-file-path)/;
+          next if skip_item($item);
           if (defined $unknown_file_word_limit) {
-            next if ++$unknown_file_word_count{$2} > $unknown_file_word_limit;
+            next if ++$unknown_file_word_count{$item} > $unknown_file_word_limit;
           }
         }
         next if should_skip_warning $warning;
