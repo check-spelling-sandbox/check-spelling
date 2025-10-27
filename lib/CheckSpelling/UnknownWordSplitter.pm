4bfd77230d20da62898a165d1216ad8acdb55169
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-27 09:28:30 -0400

    UnknownWordSplitter: Reword `slow-file`

--- lib/CheckSpelling/UnknownWordSplitter.pm
+++ lib/CheckSpelling/UnknownWordSplitter.pm
@@ -541,7 +541,7 @@ sub split_file {
   if ($@) {
     die unless $@ eq "alarm\n";
     print WARNINGS ":$.:1 ... 1, Warning - Could not parse file within time limit (slow-file)\n";
-    skip_file($temp_dir, "could not parse file within time limit (slow-file)\n");
+    skip_file($temp_dir, "it could not be parsed file within time limit (slow-file)\n");
   }
 
   close FILE;
