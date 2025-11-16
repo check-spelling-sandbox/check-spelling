9c498b4a28f34ccb44adb0e6b98adf3b94725f32
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-16 18:17:25 -0500

    Draft implementation of block begin/end
    This is a rough draft for https://github.com/check-spelling/check-spelling/wiki/Feature%3A-Block-Ignore
    
    It includes tests, but not code to trigger it from unknown-words/check-spelling.
    
    Initial file format for `block-delimiters.list`:
    
         ```block-delimiters.list
         # Description of format 1
         <begin token for format 1>
         <end token for format 1>
    
         # Description of format 2
         <begin token for format 2>
         <end token for format 2>
         ```
    
    Note that this file format has serious drawbacks and is likely to be
    replaced before this feature ships.
    
    Possible replacement file format under consideration:
    
         ```block-ignore.rules
         name: format 1
         begin-pattern: (regular-expression)
         end-pattern: (regular-expression)
         file-path-pattern: (regular-expression)
         stop-after-pattern: (regular-expression)
         ```

--- unknown-words.sh
+++ unknown-words.sh
@@ -1306,6 +1306,7 @@ define_variables() {
   patterns="$splitter_configuration/patterns.txt"
   forbidden_path="$splitter_configuration/forbidden.txt"
   candidates_path="$splitter_configuration/candidates.txt"
+  block_delimiters_path="$splitter_configuration/block-delimiters.list";
   excludes=${excludes:-$(mktemp)}
   temp_sandbox=$(mktemp -d)
   excludes_path="$temp_sandbox/excludes.txt"
@@ -2371,6 +2372,8 @@ set_up_files() {
     fi
     get_project_files line_forbidden.patterns "$forbidden_path"
     get_project_files candidate.patterns "$candidates_path"
+
+    get_project_files block-delimiters.list "$block_delimiters_path"
   else
     if [ -s "$check_extra_dictionaries_list" ]; then
       export INPUT_DICTIONARY_SOURCE_PREFIXES=$(cat "$dictionary_source_prefixes_json")
