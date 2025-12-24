5c01848b1666200b02bde6288c570a3e6cb365ed
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-24 16:47:35 -0500

    LoadEnv: Add support for retrieving config from pull request

--- lib/CheckSpelling/LoadEnv.pm
+++ lib/CheckSpelling/LoadEnv.pm
@@ -7,6 +7,8 @@ use Encode qw/decode_utf8 encode_utf8 FB_DEFAULT/;
 use YAML::PP;
 use JSON::PP;
 
+my $json_canonical = JSON::PP->new->canonical([1])->utf8;
+
 sub print_var_val {
     my ($var, $val) = @_;
     if ($var =~ /[-a-z]/) {
@@ -15,6 +17,46 @@ sub print_var_val {
     }
     return if $val eq '';
     print qq<export INPUT_$var='$val';\n>;
+}
+
+sub expect_array {
+    my ($ref, $label) = @_;
+    my $ref_kind = ref $ref;
+    if ($ref eq '') {
+        $ref = [];
+    } elsif (ref $ref ne 'ARRAY') {
+        print STDERR "'$label' should be an array (unsupported-configuration)\n";
+        $ref = [];
+    }
+    return $ref;
+}
+
+sub expect_map {
+    my ($ref, $label) = @_;
+    my $ref_kind = ref $ref;
+    if ($ref_kind eq '') {
+        $ref = {};
+    } elsif ($ref_kind ne 'HASH') {
+        print STDERR "'$label' was '$ref_kind' but should be a map (unsupported-configuration)\n";
+        $ref = {};
+    }
+    return $ref;
+}
+
+sub decode_key_val {
+    my ($key, $val) = @_;
+    my $ref_kind = ref $val;
+    if ($ref_kind eq 'HASH') {
+        $val = $json_canonical->encode($val);
+    } elsif ($ref_kind eq 'ARRAY') {
+        $val = join ' ', @$val;
+    }
+    return escape_var_val($key, $val);
+}
+
+sub array_to_map {
+    my ($array_ref) = @_;
+    return map { $_ => 1 } @$array_ref;
 }
 
 sub escape_var_val {
@@ -34,12 +76,28 @@ sub parse_config_file {
     return decode_json($base_config_data || '{}');
 }
 
+sub read_config_from_sha {
+    my ($github_head_sha, $parsed_inputs) = @_;
+    my $file = get_json_config_path($parsed_inputs);
+    open (my $config_data, '-|:raw', qq!git show '$github_head_sha':'$file' 2> /dev/null || echo '{"broken":1}'!);
+    return parse_config_file($config_data);
+}
+
+sub read_config_from_file {
+    my ($parsed_inputs) = @_;
+    open my $config_data, '<:raw', get_json_config_path($parsed_inputs);
+    return parse_config_file($config_data);
+}
+
 sub parse_inputs {
+    my ($load_config_from_key) = @_;
     my $input = $ENV{INPUTS};
     my %raw_inputs;
     if ($input) {
         %raw_inputs = %{decode_json(Encode::encode_utf8($input))};
     }
+    my $maybe_load_inputs_from = $raw_inputs{$load_config_from_key};
+    delete $raw_inputs{$load_config_from_key};
 
     my %inputs;
     for my $key (keys %raw_inputs) {
@@ -67,6 +125,7 @@ sub parse_inputs {
 
     my $parsed_inputs = {
         maybe_load_inputs_from => $maybe_load_inputs_from,
+        load_config_from_key => $load_config_from_key,
         inputs => \%inputs,
     };
     parse_action_config($parsed_inputs);
@@ -100,10 +159,145 @@ sub parse_action_config {
     $parsed_inputs->{'inputs'} = \%inputs;
 }
 
+sub get_supported_key_list {
+    my @supported_key_list = qw(
+        check_file_names
+        dictionary_source_prefixes
+        dictionary_url
+        dictionary_version
+        extra_dictionaries
+        extra_dictionary_limit
+        errors
+        notices
+        longest_word
+        lower-pattern
+        punctuation-pattern
+        upper-pattern
+        ignore-pattern
+        lower-pattern
+        not-lower-pattern
+        not-upper-or-lower-pattern
+        punctuation-pattern
+        upper-pattern
+        warnings
+    );
+    return \@supported_key_list;
+}
+
 sub get_json_config_path {
     my ($parsed_inputs) = @_;
-    my $config = $parsed_inputs->{'inputs'}{'config'} || '.github/actions/spelling';
+    my $config = $ENV{INPUT_CONFIG} || $parsed_inputs->{'inputs'}{'CONFIG'} || '.github/actions/spelling';
     return "$config/config.json";
+}
+
+sub read_project_config {
+    my ($parsed_inputs) = @_;
+    return read_config_from_file($parsed_inputs);
+}
+
+sub load_untrusted_config {
+    my ($parsed_inputs, $event_name) = @_;
+    my $maybe_load_inputs_from = $parsed_inputs->{'maybe_load_inputs_from'};
+    my $load_config_from_key = $parsed_inputs->{'load_config_from_key'};
+
+    my %supported_keys = array_to_map(get_supported_key_list);
+
+    return unless defined $maybe_load_inputs_from;
+    $maybe_load_inputs_from = decode_json $maybe_load_inputs_from unless ref $maybe_load_inputs_from eq 'HASH';
+
+    $maybe_load_inputs_from = expect_map($maybe_load_inputs_from, $load_config_from_key);
+    my %load_config_from = %$maybe_load_inputs_from;
+    my $use_pr_base_keys = 'pr-base-keys';
+    my $trust_pr_keys = 'pr-trusted-keys';
+    my $use_pr_base_key = expect_array($load_config_from{$use_pr_base_keys}, "$load_config_from_key->$use_pr_base_keys");
+    my $trust_pr_key = expect_array($load_config_from{$trust_pr_keys}, "$load_config_from_key->$use_pr_base_keys");
+    my @use_pr_base_key_list = @$use_pr_base_key;
+    my @trust_pr_key_list = @$trust_pr_key;
+    my %use_pr_base_key_map = array_to_map $use_pr_base_key if (defined $use_pr_base_key);
+    my %trust_pr_key_map = array_to_map $trust_pr_key if (defined $trust_pr_key);
+    delete $use_pr_base_key_map{''};
+    delete $trust_pr_key_map{''};
+    for my $key (keys %trust_pr_key_map) {
+        if (defined $use_pr_base_key_map{$key}) {
+            delete $trust_pr_key_map{$key};
+            print STDERR "'$key' found in both $use_pr_base_keys and $trust_pr_keys of $load_config_from_key (unsupported-configuration)\n";
+        }
+        unless (defined $supported_keys{$key}) {
+            delete $trust_pr_key_map{$key};
+            print STDERR "'$key' cannot be set in $trust_pr_keys of $load_config_from_key (unsupported-configuration)\n";
+        }
+    }
+    return unless %use_pr_base_key_map or %trust_pr_key_map;
+    if (%use_pr_base_key_map) {
+        print STDERR "... need to read base file\n";
+    }
+
+    if (%trust_pr_key_map) {
+        my ($maybe_dangerous, $local_config);
+        if (defined $event_name && $event_name eq 'pull_request_target') {
+            ($maybe_dangerous, $local_config) = (' (dangerous)', 'attacker');
+        } else {
+            ($maybe_dangerous, $local_config) = ('', 'local');
+        }
+
+        print STDERR "... will read live file$maybe_dangerous\n";
+        my %dangerous_config = %{read_project_config($parsed_inputs)};
+        for my $key (sort keys %dangerous_config) {
+            if (defined $trust_pr_key_map{$key}) {
+                my $val = $dangerous_config{$key};
+                ($key, $val) = decode_key_val($key, $val);
+                print STDERR "Trusting '$key': '$val'\n";
+                $parsed_inputs->{'inputs'}{$key} = $val;
+            } else {
+                print STDERR "Ignoring '$key' from $local_config config\n";
+            }
+        }
+    }
+
+    return unless %use_pr_base_key_map;
+
+    my $github_event_path = $ENV{GITHUB_EVENT_PATH};
+    open my $github_event_file, '<:raw', $github_event_path;
+    local $/ = undef;
+    my $github_event_data = <$github_event_file>;
+    close $github_event_file;
+    my $github_event;
+    {
+        local $@;
+        eval {
+            $github_event = decode_json ($github_event_data || '{}');
+        };
+        if ($@) {
+            print STDERR "cat $github_event_path:\n";
+            print STDERR `cat "$github_event_path"`;
+            print STDERR "\n===TEAR===\n";
+            die "::error ::decode_json failed on $github_event_path: $@\n";
+        }
+    }
+    my $github_head_sha;
+    $github_head_sha = $github_event->{'pull_request'}->{'base'}->{'sha'} if ($github_event->{'pull_request'} && $github_event->{'pull_request'}->{'base'});
+
+    my %base_config = %{read_config_from_sha($github_head_sha, $parsed_inputs)};
+    for my $key (sort keys %base_config) {
+        if (defined $use_pr_base_key_map{$key}) {
+            my ($var, $val);
+            $val = $base_config{$key};
+            ($var, $val) = decode_key_val($key, $val);
+            print STDERR "Using '$key': '$val'\n";
+            $parsed_inputs->{'inputs'}{$var} = $val;
+        } else {
+            print STDERR "Ignoring '$key' from base config\n";
+        }
+    }
+}
+
+sub load_trusted_config {
+    my ($parsed_inputs) = @_;
+    my %project_config = %{read_project_config($parsed_inputs)};
+    for my $key (keys %project_config) {
+        my ($var, $val) = decode_key_val($key, $project_config{$key});
+        $parsed_inputs->{'inputs'}{$var} = $val;
+    }
 }
 
 1;
