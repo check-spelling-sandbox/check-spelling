0a19d2db5ae9644e81748316ce1a325d0e5a9d62
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-23 21:32:36 -0500

    LoadEnv: Refactor as module

---
+++ lib/CheckSpelling/LoadEnv.pm
@@ -0,0 +1,110 @@
+#! -*-perl-*-
+
+package CheckSpelling::LoadEnv;
+
+use feature 'unicode_strings';
+use Encode qw/decode_utf8 encode_utf8 FB_DEFAULT/;
+use YAML::PP;
+use JSON::PP;
+
+sub print_var_val {
+    my ($var, $val) = @_;
+    if ($var =~ /[-a-z]/) {
+        print STDERR "Found improperly folded key in inputs '$var'\n";
+        return;
+    }
+    return if $val eq '';
+    print qq<export INPUT_$var='$val';\n>;
+}
+
+sub escape_var_val {
+    my ($var, $val) = @_;
+    $val =~ s/([\$])/\\$1/g;
+    $val =~ s/'/'"'"'/g;
+    $var = uc $var;
+    $var =~ s/-/_/g;
+    return ($var, $val);
+}
+
+sub parse_config_file {
+    my ($config_data) = @_;
+    local $/ = undef;
+    my $base_config_data = <$config_data>;
+    close $config_data;
+    return decode_json($base_config_data || '{}');
+}
+
+sub parse_inputs {
+    my $input = $ENV{INPUTS};
+    my %inputs;
+    if ($input) {
+        %inputs = %{decode_json(Encode::encode_utf8($input))};
+    }
+
+    my %input_map;
+    for my $key (keys %inputs) {
+        next unless $key;
+        my $val = $inputs{$key};
+        next unless $val ne '';
+        my $var = $key;
+        if ($val =~ /^github_pat_/) {
+            print STDERR "Censoring `$var` (unexpected-input-value)\n";
+            next;
+        }
+        next if $var =~ /\s/;
+        next if $var =~ /[-_](?:key|token)$/;
+        if ($var =~ /-/ && $inputs{$var} ne '') {
+            my $var_pattern = $var;
+            $var_pattern =~ s/-/[-_]/g;
+            my @vars = grep { /^${var_pattern}$/ && ($var ne $_) && $inputs{$_} ne '' && $inputs{$var} ne $inputs{$_} } keys %inputs;
+            if (@vars) {
+                print STDERR 'Found conflicting inputs for '.$var." ($inputs{$var}): ".join(', ', map { "$_ ($inputs{$_})" } @vars)." (migrate-underscores-to-dashes)\n";
+            }
+            $var =~ s/-/_/g;
+        }
+        ($var, $val) = escape_var_val($var, $val);
+        $input_map{$var} = $val;
+    }
+
+    my $parsed_inputs = {
+        maybe_load_inputs_from => $maybe_load_inputs_from,
+        input_map => \%input_map,
+    };
+    parse_action_config($parsed_inputs);
+    return $parsed_inputs;
+}
+
+sub parse_action_config {
+    my ($parsed_inputs) = @_;
+    my $action_yml_path = $ENV{action_yml};
+    return unless defined $action_yml_path;
+
+    my $action = YAML::PP::LoadFile($action_yml_path);
+    return unless defined $action->{inputs};
+    my %input_map = %{$parsed_inputs->{'input_map'}};
+    my %action_inputs = %{$action->{inputs}};
+    for my $key (sort keys %action_inputs) {
+        my %ref = %{$action_inputs{$key}};
+        next unless defined $ref{default};
+        next if defined $input_map{$key};
+        my $var = $key;
+        next if $var =~ /[-_](?:key|token)$/i;
+        if ($var =~ s/-/_/g) {
+            next if defined $input_map{$var};
+        }
+        my $val = $ref{default};
+        next if $val eq '';
+        ($var, $val) = escape_var_val($var, $val);
+        next if defined $input_map{$var};
+        $input_map{$var} = $val;
+    }
+    $parsed_inputs->{'input_map'} = \%input_map;
+}
+
+sub get_json_config_path {
+    my ($parsed_inputs) = @_;
+    my $config = $parsed_inputs->{'input_map'}{'config'} || '.github/actions/spelling';
+    return "$config/config.json";
+}
+
+1;
---
+++ t/LoadEnv.t
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S perl -T -w -Ilib
+
+use strict;
+use warnings;
+use utf8;
+
+use Test::More;
+use Capture::Tiny ':all';
+
+plan tests => 20;
+use_ok('CheckSpelling::LoadEnv');
+
+{
+    my ($k, $v) = CheckSpelling::LoadEnv::escape_var_val("test", "case");
+    is($k, 'TEST', 'basic key');
+    is($v, 'case', 'basic value');
+
+    ($k, $v) = CheckSpelling::LoadEnv::escape_var_val("test-ing", q<ev"c$a's\\e
+me>);
+    is($k, 'TEST_ING', 'dashed key');
+    is($v, q<ev"c\$a'"'"'s\e>."\n".'me', 'evil value');
+}
+
+my ($stdout, $stderr, @result) = capture {
+    return CheckSpelling::LoadEnv::print_var_val('HELLO', 'world');
+};
+is($stdout, q<export INPUT_HELLO='world';
+>, 'proper print_var_val out');
+is($stderr, '', 'proper print_var_val err');
+is(@result, 1, 'proper print_var_val result');
+
+($stdout, $stderr, @result) = capture {
+    return CheckSpelling::LoadEnv::print_var_val('hello', 'world');
+};
+is($stdout, '', 'improper print_var_val out');
+is($stderr, "Found improperly folded key in inputs 'hello'\n", 'improper print_var_val err');
+is(@result, 0, 'improper print_var_val result');
+
+($stdout, $stderr, @result) = capture {
+    return CheckSpelling::LoadEnv::print_var_val('HELLO', '');
+};
+is($stdout, '', 'empty print_var_val out');
+is($stderr, '', 'empty print_var_val err');
+is(@result, 0, 'empty print_var_val result');
+
+my $json = '{"hello":"world"}';
+open my $fh, '<', \$json;
+my $parsed = CheckSpelling::LoadEnv::parse_config_file($fh);
+is($parsed->{'hello'}, 'world', 'parse config file');
+
+my $not_json = '';
+open $fh, '<', \$not_json;
+my $ref = CheckSpelling::LoadEnv::parse_config_file($fh);
+is(ref $ref, 'HASH', 'parse_config_file fallback is ref');
+is(keys %{$ref}, 0, 'parse_config_file fallback has no entries');
+
+$ENV{INPUTS} = '{"hello":"world"}';
+$ENV{action_yml} = 'action.yml';
+my $parsed_input = CheckSpelling::LoadEnv::parse_inputs();
+my %parsed_inputs = %{$parsed_input};
+my $input_map = $parsed_inputs{'input_map'};
+
+like($input_map->{'DICTIONARY_URL'}, qr{https://}, 'dictionary url');
+like((join ", ", (sort keys %{$input_map})), qr{CHECK_EXTRA_DICTIONARIES.*, HELLO, .*WARNINGS}, 'input_map');
+is(CheckSpelling::LoadEnv::get_json_config_path($parsed_input), '.github/actions/spelling/config.json', 'json_config_path');
