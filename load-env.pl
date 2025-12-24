0581f3abfd07f230b8a2d8dfc763ac2316234146
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-24 16:47:35 -0500

    LoadEnv: Add support for retrieving config from pull request

--- load-env.pl
+++ load-env.pl
@@ -2,7 +2,16 @@
 use 5.022;
 use CheckSpelling::LoadEnv;
 
-my $parsed_inputs = CheckSpelling::LoadEnv::parse_inputs();
+my $load_config_from_key = 'load-config-from';
+my $parsed_inputs = CheckSpelling::LoadEnv::parse_inputs($load_config_from_key);
+
+my $event_name = $ENV{GITHUB_EVENT_NAME};
+
+if ($event_name eq 'pull_request_target') {
+    CheckSpelling::LoadEnv::load_untrusted_config($parsed_inputs, $event_name);
+} else {
+    CheckSpelling::LoadEnv::load_trusted_config($parsed_inputs);
+}
 
 my %inputs = %{$parsed_inputs->{'inputs'}};
 for my $var (sort keys %inputs) {
