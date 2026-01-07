9b6da7395053cbd62752f06f0cab4940e3020cf1
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-07 14:20:51 -0500

    call-curl: Refactor

--- unknown-words.sh
+++ unknown-words.sh
@@ -1182,8 +1182,6 @@ define_variables() {
   GITHUB_TOKEN="${GITHUB_TOKEN:-"$INPUT_GITHUB_TOKEN"}"
   if [ -n "$GITHUB_TOKEN" ]; then
     export AUTHORIZATION_HEADER="Authorization: token $GITHUB_TOKEN"
-  else
-    export AUTHORIZATION_HEADER='X-No-Authorization: Sorry About That'
   fi
 
   if  [ -n "$ACT" ] &&
@@ -1670,9 +1668,8 @@ download_or_quit_with_error() {
   fi
 }
 
-set_up_ua() {
-  export CHECK_SPELLING_VERSION="$(cat "$spellchecker/version")"
-  curl_ua="check-spelling/$CHECK_SPELLING_VERSION; $(curl --version|perl -ne '$/=undef; <>; s/\n.*//;s{ }{/};s/ .*//;print')"
+set_up_curl() {
+  . "$spellchecker/call-curl.sh"
 }
 
 limit_apt_repositories() {
@@ -1842,91 +1839,6 @@ set_up_tools() {
   check_perl_libraries
   install_tools
   set_up_jq
-}
-
-curl_auth() {
-  if [ -z "$no_curl_auth" ]; then
-    echo "$AUTHORIZATION_HEADER"
-  else
-    echo 'X-No-Authorization: Sorry About That'
-  fi
-}
-
-dump_curl_response() {
-  cat "$response_body"
-  rm -f "$response_body"
-  if [ -z "$keep_headers" ]; then
-    rm -f "$response_headers"
-  fi
-}
-
-call_curl() {
-  curl_attempt=0
-  response_headers="$(mktemp)"
-  response_body="$(mktemp)"
-  curl_output="$(mktemp)"
-  curl_url="$1"
-  shift
-  until [ $curl_attempt -ge 3 ]
-  do
-    curl \
-      "$curl_url" \
-      -D "$response_headers" \
-      -A "$curl_ua" \
-      -s \
-      -H "$(curl_auth)" \
-      "$@" \
-      -o "$response_body" \
-      > "$curl_output"
-    curl_exit_code=$?
-    if [ ! -s "$response_body" ] && [ -s "$curl_output" ]; then
-      mv "$curl_output" "$response_body"
-    fi
-    echo >> "$response_headers"
-    response_code=$(perl -e '$_=<>; $_=0 unless s#^HTTP/[\d.]+ (\d+).*#$1#;print;' "$response_headers")
-    if [ "$curl_exit_code" -ne 0 ] && [ $response_code -eq 0 ]; then
-      case $curl_exit_code in
-      2)
-        dump_curl_response
-        (
-          echo "call_curl got an exit code $curl_exit_code from curl for '$curl_url'"
-        ) >&2
-        return
-        ;;
-      3)
-        dump_curl_response
-        return
-        ;;
-      *)
-        delay=0
-        (
-          echo "call_curl got an exit code $curl_exit_code from curl for '$curl_url' and will wait for ${delay}s before retrying:"
-          cat "$response_headers"
-        ) >&2
-        ;;
-      esac
-    else
-      case "$response_code" in
-      301|302|307|308)
-        curl_url="$(perl -ne 'next unless /^location:\s*(\S+)/i; print $1' "$response_headers")"
-        delay=0
-        ;;
-      429|502|503)
-        delay="$("$calculate_delay" "$response_headers")"
-        ;;
-      *)
-        dump_curl_response
-        return
-        ;;
-      esac
-      (
-        echo "call_curl received a $response_code and will wait for ${delay}s before retrying:"
-        grep -E -i 'x-github-request-id|x-rate-limit-|retry-after' "$response_headers"
-      ) >&2
-    fi
-    sleep "$delay"
-    curl_attempt="$(( curl_attempt + 1 ))"
-  done
 }
 
 set_up_jq() {
@@ -4161,7 +4073,7 @@ if [ "$INPUT_TASK" = hash-dictionaries ]; then
   hash_dictionaries
 fi
 basic_setup
-set_up_ua
+set_up_curl
 set_up_tools
 define_variables
 set_up_reporter
