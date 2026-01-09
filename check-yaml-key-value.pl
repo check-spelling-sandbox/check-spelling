d3fdd25460a71e0dc3967d8298b3773e609c33c9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-09 07:13:36 -0500

    check-yaml-key-value.pl: Support checking for value `0`

--- check-yaml-key-value.pl
+++ check-yaml-key-value.pl
@@ -7,7 +7,7 @@ my $key = CheckSpelling::Util::get_file_from_env('KEY', undef);
 my $value = CheckSpelling::Util::get_file_from_env('VALUE', undef);
 my $message = CheckSpelling::Util::get_file_from_env('MESSAGE', '');
 my $report_match = CheckSpelling::Util::get_file_from_env('REPORT_MATCHING_YAML', '');
-exit unless $key && $value;
+exit unless $key && ($value ne '');
 my $content;
 {
   $/ = undef;
