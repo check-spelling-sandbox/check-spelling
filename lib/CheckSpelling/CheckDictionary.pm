b99765b8efc01e4d2770a3a3e5ab31b251366f13
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-29 20:32:09 -0500

    CheckDictionary: Drop file argument from process_line

--- lib/CheckSpelling/CheckDictionary.pm
+++ lib/CheckSpelling/CheckDictionary.pm
@@ -3,13 +3,13 @@
 package CheckSpelling::CheckDictionary;
 
 sub process_line {
-    my ($file, $line) = @_;
+    my ($line) = @_;
     $line =~ s/$ENV{comment_char}.*//;
     my $ignore_pattern = $ENV{INPUT_IGNORE_PATTERN} || '';
     if ($ignore_pattern ne '' && $line =~ /^.*?($ignore_pattern+)/) {
         my ($left, $right) = ($-[1] + 1, $+[1] + 1);
         my $column_range="$left ... $right";
-        return ('', "$file:$.:$column_range, Warning - Ignoring entry because it contains non-alpha characters (non-alpha-in-dictionary)\n");
+        return ('', "$column_range, Warning - Ignoring entry because it contains non-alpha characters (non-alpha-in-dictionary)\n");
     }
     return ($line, '');
 }
