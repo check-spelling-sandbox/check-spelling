f3e570cffa45bbcd65e97c0b2c8595e67c68eded
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-02 19:36:52 -0500

    update-state: Refactor strip_leading

--- update-state.sh
+++ update-state.sh
@@ -9,6 +9,9 @@ N="$n$n"
 strip_lead() {
   perl -ne 's/^\s+(\S)/$1/; print'
 }
+strip_leading() {
+  leading="$1" perl -pe 's/^ {$ENV{leading}}(.)/$1/'
+}
 strip_blanks() {
   perl -ne 'next unless /./; print'
 }
