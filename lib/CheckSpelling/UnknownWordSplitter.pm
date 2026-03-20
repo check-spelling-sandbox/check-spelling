d4427cd110fc8b0a82adc07ed8ac1fa501c7f7cc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-03-19 22:26:11 -0400

    action: Add check-homoglyphs
    The initial data is based on:
    https://github.com/codebox/homoglyph/blob/master/raw_data/chars.txt
    
    - filtered for: `^(?:[a-hA-H]|[jkJ-P]|[mn]|[p-zQ-Z])`
    - `I` and `i` are split from `^1`
    - `l` is not covered as it would collide with `1` (a more careful split could be done)

--- lib/CheckSpelling/UnknownWordSplitter.pm
+++ lib/CheckSpelling/UnknownWordSplitter.pm
@@ -24,6 +24,7 @@ use Digest::SHA;
 our $VERSION='0.1.0';
 
 my ($longest_word, $shortest_word, $word_match, $forbidden_re, $patterns_re, $candidates_re, $disable_word_collating, $check_file_names);
+my ($check_homoglyphs);
 my ($ignore_pattern, $upper_pattern, $lower_pattern, $not_lower_pattern, $not_upper_or_lower_pattern, $punctuation_pattern);
 my ($shortest, $longest) = (255, 0);
 my @forbidden_re_list;
@@ -165,7 +166,7 @@ sub load_dictionary {
   my ($dict) = @_;
   our ($word_match, $longest, $shortest, $longest_word, $shortest_word, %dictionary);
   $longest_word = CheckSpelling::Util::get_val_from_env('INPUT_LONGEST_WORD', undef);
-  $shortest_word = CheckSpelling::Util::get_val_from_env('INPUT_SHORTEST_WORD', undef);
+  $shortest_word = CheckSpelling::Util::get_val_from_env('INPUT_SHORTEST_WORD', 0);
   our ($ignore_pattern, $upper_pattern, $lower_pattern, $not_lower_pattern, $not_upper_or_lower_pattern, $punctuation_pattern);
   $ignore_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_IGNORE_PATTERN', q<[^a-zA-Z']>);
   $upper_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_UPPER_PATTERN', '[A-Z]');
@@ -173,6 +174,18 @@ sub load_dictionary {
   $not_lower_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_NOT_LOWER_PATTERN', '[^a-z]');
   $not_upper_or_lower_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_NOT_UPPER_OR_LOWER_PATTERN', '[^A-Za-z]');
   $punctuation_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_PUNCTUATION_PATTERN', q<'>);
+  our $check_homoglyphs = CheckSpelling::Util::get_val_from_env('INPUT_CHECK_HOMOGLYPHS', 0);
+  if ($check_homoglyphs && $check_homoglyphs !~ /false/i) {
+    my $homoglyph_list_path = CheckSpelling::Util::get_file_from_env_utf8('homoglyph_list_path', '/dev/null');
+    if (-s $homoglyph_list_path) {
+      use CheckSpelling::Homoglyph;
+      CheckSpelling::Homoglyph::init($homoglyph_list_path);
+    } else {
+      $check_homoglyphs = 0;
+    }
+  } else {
+    $check_homoglyphs = 0;
+  }
   %dictionary = ();
 
   open(my $dict_fh, '<:utf8', $dict);
@@ -584,7 +597,29 @@ sub split_file {
       # This is to make it easier to deal w/ rules:
       s/^/ /;
       my %unrecognized_line_items = ();
-      my ($new_words, $new_unrecognized) = split_line($_, \%unique, \%unique_unrecognized, \%unrecognized_line_items, $warnings_fh);
+      our $check_homoglyphs;
+      if ($check_homoglyphs) {
+        my $check_line_for_homoglyphs = $_;
+        my $homoglyphs = $CheckSpelling::Homoglyph::homoglyphs;
+        # problematic characters: `\\`, `-`, `]`
+        $homoglyphs =~ s/([-\\\]])/\\$1/g;
+        $homoglyphs = "[$homoglyphs]";
+        our ($longest_word, $shortest_word);
+        my $longest_word_string = defined $longest_word && ($longest_word =~ /^\d+$/) ? $longest_word : '';
+        my $dollar = '$';
+        my $homoglyph_re = "(?=(?:${homoglyphs}|(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})){${shortest_word},${longest_word_string}}(?:${not_upper_or_lower_pattern}|${dollar}))((?:${upper_pattern}|${lower_pattern})+${homoglyphs}(?:(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})|${homoglyphs})*|${homoglyphs}+(?:${upper_pattern}|${lower_pattern})(?:(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})|${homoglyphs})*)";
+        while ($check_line_for_homoglyphs =~ /((?=(?:${homoglyphs}|(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})){${shortest_word},${longest_word_string}}(?:${not_upper_or_lower_pattern}|${dollar}))((?:${upper_pattern}|${lower_pattern})+${homoglyphs}(?:(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})|${homoglyphs})*|${homoglyphs}+(?:${upper_pattern}|${lower_pattern})(?:(?:${upper_pattern}|${lower_pattern}|${punctuation_pattern})|${homoglyphs})*))/g) {
+          my ($token, $token_raw, $begin, $end) = ($1, $1, $-[0], $+[0]);
+          $token =~ s/($homoglyphs)/$CheckSpelling::Homoglyph::homoglyph_to_glyph{$1}/g;
+          if (defined $dictionary{$token}) {
+            my $token_raw = CheckSpelling::Util::wrap_in_backticks($token_raw);
+            my $token = CheckSpelling::Util::wrap_in_backticks($token);
+            my $wrapped = "$token_raw should probably be $token (homoglyph-word)";
+            print $warnings_fh ":$.:$begin ... $end, Error - $wrapped\n";
+          }
+        }
+      }
+      my ($new_words, $new_unrecognized) = split_line($_, \%unique, \%unique_unrecognized, \%unrecognized_line_items);
       $words += $new_words;
       $unrecognized += $new_unrecognized;
       my $line_length = length($raw_line);
--- t/unknown-words/output/summary.md
+++ t/unknown-words/output/summary.md
@@ -136,13 +136,14 @@ These forbidden patterns matched content:
 
 </details>
 
-<details><summary>Errors, Warnings, and Notices ❌ (4)</summary>
+<details><summary>Errors, Warnings, and Notices ❌ (5)</summary>
 
 #### See the [📜action log](GITHUB_SERVER_URL/GITHUB_REPOSITORY_OWNER/GITHUB_REPOSITORY_NAME/actions/runs/GITHUB_RUN_ID) or 📝 job summary for details.
 
 [❌ Errors, Warnings, and Notices](https://docs.check-spelling.dev/Event-descriptions) | Count
 -|-
 [❌ forbidden-pattern](https://docs.check-spelling.dev/Event-descriptions#forbidden-pattern) | 5
+[⚠️ homoglyph-word](https://docs.check-spelling.dev/Event-descriptions#homoglyph-word) | 1
 [⚠️ ignored-expect-variant](https://docs.check-spelling.dev/Event-descriptions#ignored-expect-variant) | 1
 [⚠️ non-alpha-in-dictionary](https://docs.check-spelling.dev/Event-descriptions#non-alpha-in-dictionary) | 1
 [ℹ️ unused-config-file](https://docs.check-spelling.dev/Event-descriptions#unused-config-file) | 1
@@ -161,6 +162,13 @@ note|path
 `documentation.file` matches a line_forbidden.patterns rule: Should be `documentation.pdf` - `\bdocumentation\.file\b` | unknown-words/input/sample.file:5
 `logo.ico` matches a line_forbidden.patterns rule: Should be `logo.png` - `\blogo\.ico\b` | unknown-words/input/sample.file:7
 `sample.file` matches a line_forbidden.patterns rule: Should be `sample-file.txt` - `\bsample\.file\b` | unknown-words/input/sample.file:1
+</details>
+
+<details><summary>📂 homoglyph-word</summary>
+
+note|path
+-|-
+`brandıng` should probably be `branding` | unknown-words/input/sample.file:7
 </details>
 
 <details><summary>📂 ignored-expect-variant</summary>
