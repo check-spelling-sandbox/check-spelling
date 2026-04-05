check-spelling-sandbox/autotest-check-spelling

unknown-words: Improve input.task handling
- Error for unrecognized input.task
- Report warnings for alternate tasks

unknown-words.sh
@@ -42,19 +42,59 @@ workflow_skipped() {
   exit 0
 }
 
+report_cached_warning_output() {
+  warning_output="$(mktemp)"
+  (
+    warning_output_unsorted="$data_dir/warning_output_unsorted.txt"
+    if [ -s "$warning_output_unsorted" ]; then
+      cat "$data_dir/warning_output_unsorted.txt" >> "$early_warnings"
+    fi
+  )
+  cat /dev/null |
+    warning_output="$warning_output" \
+    counter_summary="$counter_summary_file" \
+    candidate_summary="$candidate_summary" \
+    early_warnings="$early_warnings" \
+    "$word_collator" |\
+    "$strip_word_collator_suffix" > "$run_output"
+  cat "$warning_output"
+}
+
 dispatcher() {
   if [ -n "$INPUT_EVENT_ALIASES" ]; then
     GITHUB_EVENT_NAME="$(echo "$INPUT_EVENT_ALIASES" | jq -r ".$GITHUB_EVENT_NAME // $Q$GITHUB_EVENT_NAME$Q")"
   fi
   case "$INPUT_TASK" in
+    '')
+      ;;
     comment|collapse_previous_comment)
       if ! to_boolean "$INPUT_POST_COMMENT"; then
         INPUT_POST_COMMENT=1
       fi
+      report_cached_warning_output
       comment_task
     ;;
     pr_head_sha)
       pr_head_sha_task
+    ;;
+    *)
+      early_warning_count=$(line_count < "$early_warnings")
+      unrecognized_task_message='Error - Unsupported task `'$INPUT_TASK'` (unsupported-task)'
+      if [ -e "$workflow_path" ]; then
+        workflow_path_for_input_task="$workflow_path"
+      else
+        workflow_path_for_input_task="$INPUT_INTERNAL_STATE_DIRECTORY/workflow.yml"
+      fi
+      if [ -s "$workflow_path_for_input_task" ]; then
+        KEY="jobs$n$THIS_GITHUB_JOB_ID${n}steps${n}with${n}task" \
+        VALUE="$INPUT_TASK" \
+        MESSAGE="$unrecognized_task_message" \
+        file="$workflow_path" \
+        check_yaml_key_value "$workflow_path_for_input_task"
+      fi
+      if [ $early_warning_count == $(line_count < "$early_warnings") ]; then
+        echo "$workflow_path:0:0 ... 0, $unrecognized_task_message" >> "$early_warnings"
+      fi
     ;;
   esac
   case "$GITHUB_EVENT_NAME" in
@@ -288,6 +328,22 @@ load_env() {
       fi
       echo "$check_spelling_with"
     )"
+  fi
+  repository_and_workflow_path_without_ref=${GITHUB_WORKFLOW_REF%%@*}
+  private_workflow_path=${repository_and_workflow_path_without_ref#*/*/}
+  if [ "$GITHUB_EVENT_NAME" = 'pull_request_target' ]; then
+    default_branch=$(jq -r '.repository.default_branch // empty' "$GITHUB_EVENT_PATH")
+    if [ -n "$default_branch" ] &&
+      git fetch origin "$GITHUB_WORKFLOW_SHA:refs/private/workflow-ref" --depth 1; then
+      if [ -n "$private_workflow_path" ]; then
+        git show "refs/private/default-branch:$private_workflow_path" > "$retrieved_default_workflow_file" || true
+      fi
+    fi
+  fi
+  if [ ! -s "$retrieved_default_workflow_file" ] &&
+     [ -s "$private_workflow_path" ] &&
+     [ ! -L "$private_workflow_path" ]; then
+     cp "$private_workflow_path" "$retrieved_default_workflow_file"
   fi
   action_yml="$spellchecker/action.yml" "$spellchecker/wrappers/load-env" > "$input_variables"
   . "$input_variables"
@@ -1116,6 +1172,7 @@ define_variables() {
   fi
   . "$spellchecker/update-state.sh"
   action_workflow_path_file="$(mktemp)"
+  retrieved_default_workflow_file="$(mktemp)"
   workflow_path=$(get_workflow_path)
   load_env
   GITHUB_TOKEN="${GITHUB_TOKEN:-"$INPUT_GITHUB_TOKEN"}"
@@ -1170,6 +1227,12 @@ define_variables() {
   early_warnings="$data_dir/early_warnings.txt"
   severity_level="$data_dir/severity_level.txt"
   severity_list="$data_dir/severity_list.txt"
+  default_workflow_file="$data_dir/workflow.yml"
+
+  if  [ ! -s "$default_workflow_file" ] &&
+      [ -s "$retrieved_default_workflow_file" ]; then
+    cp "$retrieved_default_workflow_file" "$default_workflow_file"
+  fi
 
   bucket="${INPUT_BUCKET:-"$bucket"}"
   project="${INPUT_PROJECT:-"$project"}"
@@ -2551,6 +2614,7 @@ build_file_list() {
 
 run_spell_check() {
   if [ "$INPUT_TASK" != 'spelling' ]; then
+    report_cached_warning_output
     return
   fi
   echo "started-at=$(perl -e 'use POSIX qw(strftime);

check-spelling-sandbox/check-spelling

unknown-words.sh
@@ -42,19 +42,59 @@ workflow_skipped() {
   exit 0
 }
 
+report_cached_warning_output() {
+  warning_output="$(mktemp)"
+  (
+    warning_output_unsorted="$data_dir/warning_output_unsorted.txt"
+    if [ -s "$warning_output_unsorted" ]; then
+      cat "$data_dir/warning_output_unsorted.txt" >> "$early_warnings"
+    fi
+  )
+  cat /dev/null |
+    warning_output="$warning_output" \
+    counter_summary="$counter_summary_file" \
+    candidate_summary="$candidate_summary" \
+    early_warnings="$early_warnings" \
+    "$word_collator" |\
+    "$strip_word_collator_suffix" > "$run_output"
+  cat "$warning_output"
+}
+
 dispatcher() {
   if [ -n "$INPUT_EVENT_ALIASES" ]; then
     GITHUB_EVENT_NAME="$(echo "$INPUT_EVENT_ALIASES" | jq -r ".$GITHUB_EVENT_NAME // $Q$GITHUB_EVENT_NAME$Q")"
   fi
   case "$INPUT_TASK" in
+    '')
+      ;;
     comment|collapse_previous_comment)
       if ! to_boolean "$INPUT_POST_COMMENT"; then
         INPUT_POST_COMMENT=1
       fi
+      report_cached_warning_output
       comment_task
     ;;
     pr_head_sha)
       pr_head_sha_task
+    ;;
+    *)
+      early_warning_count=$(line_count < "$early_warnings")
+      unrecognized_task_message='Error - Unsupported task `'$INPUT_TASK'` (unsupported-task)'
+      if [ -e "$workflow_path" ]; then
+        workflow_path_for_input_task="$workflow_path"
+      else
+        workflow_path_for_input_task="$INPUT_INTERNAL_STATE_DIRECTORY/workflow.yml"
+      fi
+      if [ -s "$workflow_path_for_input_task" ]; then
+        KEY="jobs$n$THIS_GITHUB_JOB_ID${n}steps${n}with${n}task" \
+        VALUE="$INPUT_TASK" \
+        MESSAGE="$unrecognized_task_message" \
+        file="$workflow_path" \
+        check_yaml_key_value "$workflow_path_for_input_task"
+      fi
+      if [ $early_warning_count == $(line_count < "$early_warnings") ]; then
+        echo "$workflow_path:0:0 ... 0, $unrecognized_task_message" >> "$early_warnings"
+      fi
     ;;
   esac
   case "$GITHUB_EVENT_NAME" in
@@ -292,6 +332,22 @@ load_env() {
       fi
       echo "$check_spelling_with"
     )"
+  fi
+  repository_and_workflow_path_without_ref=${GITHUB_WORKFLOW_REF%%@*}
+  private_workflow_path=${repository_and_workflow_path_without_ref#*/*/}
+  default_branch=$(jq -r '.repository.default_branch // empty' "$GITHUB_EVENT_PATH")
+  if [ "$GITHUB_EVENT_NAME" = 'pull_request_target' ]; then
+    if [ -n "$default_branch" ] &&
+      git fetch origin "$GITHUB_WORKFLOW_SHA:refs/private/workflow-ref" --depth 1; then
+      if [ -n "$private_workflow_path" ]; then
+        git show "refs/private/workflow-ref:$private_workflow_path" > "$retrieved_default_workflow_file" || true
+      fi
+    fi
+  fi
+  if [ ! -s "$retrieved_default_workflow_file" ] &&
+     [ -s "$private_workflow_path" ] &&
+     [ ! -L "$private_workflow_path" ]; then
+     cp "$private_workflow_path" "$retrieved_default_workflow_file"
   fi
   action_yml="$spellchecker/action.yml" "$spellchecker/wrappers/load-env" > "$input_variables"
   . "$input_variables"
@@ -1120,6 +1176,7 @@ define_variables() {
   fi
   . "$spellchecker/update-state.sh"
   action_workflow_path_file="$(mktemp)"
+  retrieved_default_workflow_file="$(mktemp)"
   workflow_path=$(get_workflow_path)
   load_env
   GITHUB_TOKEN="${GITHUB_TOKEN:-"$INPUT_GITHUB_TOKEN"}"
@@ -1174,6 +1231,12 @@ define_variables() {
   early_warnings="$data_dir/early_warnings.txt"
   severity_level="$data_dir/severity_level.txt"
   severity_list="$data_dir/severity_list.txt"
+  default_workflow_file="$data_dir/workflow.yml"
+
+  if  [ ! -s "$default_workflow_file" ] &&
+      [ -s "$retrieved_default_workflow_file" ]; then
+    cp "$retrieved_default_workflow_file" "$default_workflow_file"
+  fi
 
   bucket="${INPUT_BUCKET:-"$bucket"}"
   project="${INPUT_PROJECT:-"$project"}"
@@ -2556,6 +2619,7 @@ build_file_list() {
 
 run_spell_check() {
   if [ "$INPUT_TASK" != 'spelling' ]; then
+    report_cached_warning_output
     return
   fi
   echo "started-at=$(perl -e 'use POSIX qw(strftime);