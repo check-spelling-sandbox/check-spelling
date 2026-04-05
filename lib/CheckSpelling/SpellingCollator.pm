check-spelling-sandbox/autotest-check-spelling

SpellingCollator: Improve punctuation folding handling

lib/CheckSpelling/SpellingCollator.pm
@@ -65,6 +65,7 @@ sub skip_item {
   my ($word) = @_;
   return 1 if expect_item($word, 1);
   my $key = lc $word;
+  $key =~ s/’/'/g;
   return 2 if expect_item($key, 2);
   if ($key =~ /.s$/) {
     if ($key =~ /ies$/) {

check-spelling-sandbox/check-spelling

lib/CheckSpelling/SpellingCollator.pm
@@ -38,6 +38,7 @@ sub maybe {
 my %expected = ();
 sub expect_item {
   my ($item, $value) = @_;
+  $item =~ s/’/'/g;
   our %expected;
   my $next;
   if (defined $expected{$item}) {